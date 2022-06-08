25ef1ec5c17bcc238e43deabe7af20c014851014
Atacan Ozkan <atacanozkan@gmail.com>
2022-06-08 22:42:11 +0300

    Dictionary structure implemented.

--- MyDictionary/MyDictionary.cs
+++ MyDictionary/MyDictionary.cs
@@ -4,7 +4,58 @@ using System.Text;
 
 namespace MyDictionary
 {
-    class MyDictionary
+    class MyDictionary<TKey, TValue>
     {
+        TKey[] keys;
+        TValue[] values;
+
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                for (int i = 0; i < keys.Length; i++)
+                    if (keys[i].Equals(key))
+                        return values[i];
+                throw new Exception("There is no such a key in the dictionary");
+            }
+            set
+            {
+                for (int i = 0; i < keys.Length; i++)
+                    if (keys[i].Equals(key))
+                    {
+                        values[i] = value;
+                        return;
+                    }
+                throw new Exception("There is no such a key in the dictionary");
+            }
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            for (int i = 0; i < keys.Length; i++)
+                if (keys[i].Equals(key))
+                    throw new Exception("The key is already defined in the dictionary");
+
+            TKey[] keysTemp = keys;
+            TValue[] valuesTemp = values;
+
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < keysTemp.Length; i++)
+            {
+                keys[i] = keysTemp[i];
+                values[i] = valuesTemp[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length-1] = value;
+        }
     }
 }
--- MyDictionary/Program.cs
+++ MyDictionary/Program.cs
@@ -6,7 +6,19 @@ namespace MyDictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MyDictionary<string, int> myDictionary = new MyDictionary<string, int>();
+            myDictionary.Add("Bir", 1);
+            myDictionary.Add("İki", 2);
+            myDictionary.Add("Üç", 3);
+
+            //myDictionary.Add("Bir", 10);
+
+            Console.WriteLine(myDictionary["Bir"]);
+            myDictionary["Bir"] = 10;
+            Console.WriteLine(myDictionary["Bir"]);
+
+            Console.WriteLine(myDictionary["On"]);
+            myDictionary["On"] = 10;
         }
     }
 }
